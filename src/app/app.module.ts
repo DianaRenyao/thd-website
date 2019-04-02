a406eb40b285d569181756324629769f55c89509
Lin Yinfeng <lin.yinfeng@outlook.com>
2019-04-02 12:23:41 +0800

    implement basic login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,47 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material';
+import { MatCardModule, MatIconModule, MatInputModule, MatTabsModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { UserIndicatorComponent } from './user-indicator/user-indicator.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ApiInterceptor, JwtInterceptor } from './_interceptors';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CreditsComponent } from './credits/credits.component';
+import { AlertComponent } from './alert/alert.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserIndicatorComponent,
+    LoginComponent,
+    HomeComponent,
+    CreditsComponent,
+    AlertComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatTabsModule,
+    MatIconModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/user-indicator/user-indicator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-indicator',
+  templateUrl: './user-indicator.component.html',
+  styleUrls: ['./user-indicator.component.scss']
+})
+export class UserIndicatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
