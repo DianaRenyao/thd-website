b51d878d6c213a505fb1d52b7c4324b24b53dc45
Lin Yinfeng <lin.yinfeng@outlook.com>
2019-04-07 11:57:36 +0800

    merge changes and small fixes

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { StudentRegisterMessage, UserInfoMessage } from '../_models';
-import {TeacherRegisterMessage} from '../_models/teacher-register-message';
-import {of} from 'rxjs/internal/observable/of';
+import { StudentMessage, StudentRegisterMessage, TeacherMessage } from '../_models';
+import { TeacherRegisterMessage } from '../_models/teacher-register-message';
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +14,15 @@ export class UserService {
   ) {
   }
 
-  register(registerMessage: StudentRegisterMessage): Observable<UserInfoMessage> {
-    return this.http.post<UserInfoMessage>('students', registerMessage);
+  register(registerMessage: StudentRegisterMessage): Observable<StudentMessage> {
+    return this.http.post<StudentMessage>('students', registerMessage);
   }
-  addTeacher(registerMessage: TeacherRegisterMessage): Observable<UserInfoMessage> {
-    return this.http.post<UserInfoMessage>('teachers', registerMessage);
+
+  addTeacher(registerMessage: TeacherRegisterMessage): Observable<TeacherMessage> {
+    return this.http.post<TeacherMessage>('teachers', registerMessage);
   }
-  getAllTeachers(): Observable<UserInfoMessage[]> {
-    return this.http.get<UserInfoMessage[]>('teachers');
+
+  getAllTeachers(): Observable<TeacherMessage[]> {
+    return this.http.get<TeacherMessage[]>('teachers');
   }
 }
