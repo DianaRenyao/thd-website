bc9912075f4b22f50b011e3b5476eb3829366dd0
Lin Yinfeng <lin.yinfeng@outlook.com>
2019-04-15 20:01:20 +0800

    beatify teachers home

--- src/app/teacher-home/teacher-home-routing.module.ts
+++ src/app/teacher-home/teacher-home-routing.module.ts
@@ -1,9 +1,9 @@
-import {Routes} from '@angular/router';
-import {CourseManagementComponent} from './course-management/course-management.component';
-import {NoticeManagementComponent} from './notice-management/notice-management.component';
-import {ApplicationManagementComponent} from './application-management/application-management.component';
-import {CourseDetailManagementComponent} from './course-detail-management/course-detail-management.component';
-import {AddCourseComponent} from './course-management/add-course/add-course.component';
+import { Routes } from '@angular/router';
+import { CourseManagementComponent } from './course-management/course-management.component';
+import { NoticeManagementComponent } from './notice-management/notice-management.component';
+import { ApplicationManagementComponent } from './application-management/application-management.component';
+import { CourseDetailManagementComponent } from './course-detail-management/course-detail-management.component';
+import { AddCourseComponent } from './add-course/add-course.component';
 
 export const teacherHomeChildRoutes: Routes = [
   { path: '', redirectTo: 'notice-management', pathMatch: 'full' },
