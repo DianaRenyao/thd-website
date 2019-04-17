9f10f8b1c846be38107d8b1e0be302ef51e332ef
Lin Yinfeng <lin.yinfeng@outlook.com>
2019-04-17 17:18:15 +0800

    synchronize error codes

--- src/app/_services/service-error-message.service.ts
+++ src/app/_services/service-error-message.service.ts
@@ -45,6 +45,8 @@ export class ServiceErrorMessageService {
         return '课程不存在';
       case ErrorCode.GET_APPLICATION_NO_SUCH_APPLICATION:
         return '申请不存在';
+      case ErrorCode.GET_APPLICATION_NO_SUCH_USER:
+        return '用户不存在';
       case ErrorCode.POST_APPLICATION_NO_SUCH_COURSE:
         return '课程不存在';
       case ErrorCode.POST_APPLICATION_COURSE_CAN_NOT_BE_APPLIED:
@@ -55,6 +57,66 @@ export class ServiceErrorMessageService {
         return '申请不存在';
       case ErrorCode.PUT_APPLICATION_CAN_NOT_REJECT_APPLICATION_ALREADY_PASSED:
         return '无法拒绝已经通过的申请';
+      case ErrorCode.POST_CHAPTER_CAN_NOT_BE_INSERTED:
+        return '无法插入章节';
+      case ErrorCode.POST_CHAPTER_COURSE_DO_NOT_EXISTS:
+        return '课程不存在';
+      case ErrorCode.PUT_CHAPTER_NO_SUCH_CHAPTER:
+        return '修改的章节不存在';
+      case ErrorCode.DELETE_CHAPTER_COURSE_DO_NOT_EXISTS:
+        return '课程不存在';
+      case ErrorCode.POST_SECTION_CAN_NOT_BE_INSERTED:
+        return '小节不存在';
+      case ErrorCode.POST_SECTION_CHAPTER_DO_NOT_EXISTS:
+        return '章节不存在';
+      case ErrorCode.POST_SECTION_COURSE_DO_NOT_EXISTS:
+        return '课程不存在';
+      case ErrorCode.PUT_SECTION_NO_SUCH_SECTION:
+        return '小节不存在';
+      case ErrorCode.PUT_SECTION_NO_SUCH_CHAPTER:
+        return '章节不存在';
+      case ErrorCode.DELETE_SECTION_COURSE_DO_NOT_EXISTS:
+        return '课程不存在';
+      case ErrorCode.POST_STATIC_FILE_FAILED_TO_STORE:
+        return '无法存储该文件数据';
+      case ErrorCode.POST_STATIC_FILE_FAILED_TO_INSERT_DB:
+        return '无法存储该文件信息';
+      case ErrorCode.POST_STATIC_FILE_UNACCEPTABLE_FILE_TYPE:
+        return '不支持的文件类型';
+      case ErrorCode.GET_STATIC_FILE_FAILED_TO_DOWNLOAD_FILE:
+        return '文件不存在';
+      case ErrorCode.DELETE_STATIC_FILE_NO_SUCH_FILE:
+        return '文件不存在';
+      case ErrorCode.DELETE_STATIC_FILE_FAILED_DELETE:
+        return '删除文件失败';
+      case ErrorCode.PUT_SECTION_FILE_NO_SUCH_COURSE:
+        return '课程不存在';
+      case ErrorCode.PUT_SECTION_FILE_NO_SUCH_SECTION:
+        return '小节不存在';
+      case ErrorCode.PUT_SECTION_FILE_NO_SUCH_FILE:
+        return '文件不存在';
+      case ErrorCode.DELETE_SECTION_FILE_NO_SUCH_COURSE:
+        return '课程不存在';
+      case ErrorCode.DELETE_SECTION_FILE_NO_SUCH_FILE:
+        return '文件不存在';
+      case ErrorCode.GET_EXAM_STUDENT_NOT_ALLOW:
+        return '被禁止的访问';
+      case ErrorCode.GET_EXAM_COURSE_DO_NOT_EXISTS:
+        return '考试不存在';
+      case ErrorCode.POST_EXAM_COURSE_DO_NOT_EXISTS:
+        return '课程不存在';
+      case ErrorCode.POST_EXAM_CHAPTER_DO_NOT_EXISTS:
+        return '章节不存在';
+      case ErrorCode.POST_EXAM_ID_WRONG:
+        return '课程 ID 错误';
+      case ErrorCode.POST_EXAM_SCORE_ALREADY_EXISTS:
+        return '已经存在的考试'; // TODO what does it mean?
+      case ErrorCode.POST_EXPERIMENT_NO_SUCH_COURSE:
+        return '课程不存在';
+      case ErrorCode.POST_EXPERIMENT_FILE_NO_SUCH_EXPERIMENT:
+        return '实验不存在';
+      case ErrorCode.POST_EXPERIMENT_FILE_NO_SUCH_FILE:
+        return '文件不存在';
       default:
         return '发生了未知错误';
     }
